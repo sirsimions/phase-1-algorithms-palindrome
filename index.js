88ce39b3de0cf04c6561abb7eae184bc262b56e7
Simion <simion.okola@student.moringaschool.com>
2022-08-26 03:10:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    let wordArray = word.split("");
+    let invertedArray = wordArray.reverse();
+    let reversedWord = invertedArray.join("");
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+}
 }
 
+
 /* 
-  Add your pseudocode here
+  split the word into characters
+  reverse it
+  convert it back to normal word (without splitting)
 */
 
 /*
-  Add written explanation of your solution here
+  Once a reverse of the word is achieved, the original word can be compared too to it to acertain if they read the same from left or right
 */
 
 // You can run `node index.js` to view these console logs
